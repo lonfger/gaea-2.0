3b6fd508f2c40b4af20fd6a47ba7ce9040e3f59f
longfengzhu <longfengzhu@info.easeus.com.cn>
2025-01-07 13:48:05 +0800

    优化

--- main.js
+++ main.js
@@ -1,160 +1,206 @@
-const fs = require('fs').promises;
-const { HttpsProxyAgent } = require('https-proxy-agent');
-const path = require('path');
-const crypto = require('crypto');
-const fakeUa = require('fake-useragent');
+import { promises as fs } from 'fs'
+import pkg from 'https-proxy-agent'
+import fetch from 'node-fetch'
+const { HttpsProxyAgent } = pkg
 
-function getRandomAcceptLanguage () {
-  const languages = ['en', 'es', 'fr', 'de', 'zh', 'ja', 'ko', 'it', 'pt', 'ru'];
-  const regions = ['', '-US', '-GB', '-CN', '-FR', '-DE', '-JP', '-KR', '-RU', '-BR'];
-  const weights = Array.from({ length: 10 }, (_, i) => (10 - i) / 10);  // 权重从 1.0 到 0.1
+import { randomUUID } from 'crypto'
+import readline from 'readline'
 
-  let result = [];
-  const count = Math.floor(Math.random() * 5) + 1;  // 随机生成1到5种语言组合
+import fakeUa from 'fake-useragent'
+
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+function getRandomAcceptLanguage() {
+  const languages = ['en', 'es', 'fr', 'de', 'zh', 'ja', 'ko', 'it', 'pt', 'ru']
+  const regions = [
+    '',
+    '-US',
+    '-GB',
+    '-CN',
+    '-FR',
+    '-DE',
+    '-JP',
+    '-KR',
+    '-RU',
+    '-BR',
+  ]
+  const weights = Array.from({ length: 10 }, (_, i) => (10 - i) / 10) // 权重从 1.0 到 0.1
+
+  let result = []
+  const count = Math.floor(Math.random() * 5) + 1 // 随机生成1到5种语言组合
 
   for (let i = 0; i < count; i++) {
-    const lang = languages[Math.floor(Math.random() * languages.length)];
-    const region = regions[Math.floor(Math.random() * regions.length)];
-    const quality = weights[Math.floor(Math.random() * weights.length)].toFixed(1);
+    const lang = languages[Math.floor(Math.random() * languages.length)]
+    const region = regions[Math.floor(Math.random() * regions.length)]
+    const quality =
+      weights[Math.floor(Math.random() * weights.length)].toFixed(1)
 
-    const locale = lang + region;
-    const langQuality = locale + (quality < 1 ? `;q=${quality}` : '');
+    const locale = lang + region
+    const langQuality = locale + (quality < 1 ? `;q=${quality}` : '')
     if (!result.includes(langQuality)) {
-      result.push(langQuality);
+      result.push(langQuality)
     }
   }
 
-  return result.join(', ');
+  return result.join(', ')
 }
-async function main (accessToken, id8, proxyPath, jsonPath) {
-  let headers = {
-    'Accept': 'application/json, text/plain, */*',
-    'origin': 'chrome-extension://cpjicfogbgognnifjgmenmaldnmeeeib',
-    'Content-Type': 'application/json',
-    'Authorization': `Bearer ${accessToken}`,
-    'User-Agent': fakeUa(),
-    'Accept-Language': getRandomAcceptLanguage()
-  };
-  const browserIdFilePath = path.join(__dirname, jsonPath);
-  async function coday (url, method, payloadData = null, proxy) {
-    try {
-      const agent = new HttpsProxyAgent(proxy);
-      let response;
-      const options = {
-        method: method,
-        headers: headers,
-        agent: agent
-      };
+async function question(query) {
+  return new Promise((resolve) =>
+    rl.question(query, (answer) => resolve(answer))
+  )
+}
 
-      if (method === 'POST') {
-        options.body = JSON.stringify(payloadData);
-        response = await fetch(url, options);
-      } else {
-        response = await fetch(url, options);
+async function getBrowserIds(browserIdsFilePath) {
+  try {
+    const data = await fs.readFile(browserIdsFilePath, 'utf-8')
+    return JSON.parse(data)
+  } catch (error) {
+    return {}
+  }
+}
+
+async function saveBrowserIds(browserIds, browserIdsFilePath) {
+  try {
+    await fs.writeFile(
+      browserIdsFilePath,
+      JSON.stringify(browserIds, null, 2),
+      'utf-8'
+    )
+    console.log('已将浏览器ID保存到文件。')
+  } catch (error) {
+    console.error('保存浏览器ID出错:', error)
+  }
+}
+
+async function getBrowserIdForProxy(proxy, browserIdsFilePath) {
+  const browserIds = await getBrowserIds(browserIdsFilePath)
+  if (browserIds[proxy]) {
+    console.log(`使用现有的 browser_id ${proxy} 的认证`)
+    return browserIds[proxy]
+  } else {
+    const newBrowserId = randomUUID()
+    browserIds[proxy] = newBrowserId
+    await saveBrowserIds(browserIds, browserIdsFilePath)
+    console.log(`为代理 ${proxy} 生成新 browser_id: ${newBrowserId}`)
+    return newBrowserId
+  }
+}
+
+function getCurrentTimestamp() {
+  return Math.floor(Date.now() / 1000)
+}
+
+async function pingProxy(proxy, browserId, uid,browserIdsFilePath, headers) {
+  const timestamp = getCurrentTimestamp()
+  const data = {
+    uid,
+    browser_id: browserId,
+    timestamp,
+    version: '1.0.0',
+  }
+
+  while (true) {
+    try {
+      const response = await request(
+        'https://api.aigaea.net/api/network/ping',
+        'POST',
+        data,
+        proxy,
+        headers
+      )
+      console.log(`代理 ${proxy} 的 ping 成功:`, response)
+      if (response.data && response.data.score < 50) {
+        console.log(`代理 ${proxy} 的得分低于 50，正在重新认证...`)
+        await authProxy(proxy, browserIdsFilePath, headers)
+        break
       }
-
-      return await response.json();
     } catch (error) {
-      console.error('Error with proxy:', proxy);
+      console.error(`代理 ${proxy} 的 ping 失败:`, error)
     }
+    await new Promise((resolve) => setTimeout(resolve, 10000))
   }
-  function generateBrowserId () {
-    const rdm = crypto.randomUUID().slice(8);
-    const browserId = `${id8}${rdm}`;
-    return browserId;
-  }
-  async function loadBrowserIds () {
-    try {
-      const data = await fs.readFile(browserIdFilePath, 'utf-8');
-      return JSON.parse(data);
-    } catch (error) {
-      return {};
-    }
-  }
-  async function saveBrowserIds (browserIds) {
-    try {
-      await fs.writeFile(browserIdFilePath, JSON.stringify(browserIds, null, 2), 'utf-8');
-      console.log('Browser IDs saved to file.');
-    } catch (error) {
-      console.error('Error saving browser IDs:', error);
-    }
-  }
-  async function getBrowserId (proxy) {
-    const browserIds = await loadBrowserIds();
-    if (browserIds[proxy]) {
-      console.log(`Using existing browser_id for proxy ${proxy}`);
-      return browserIds[proxy];
-    } else {
-      const newBrowserId = generateBrowserId();
-      browserIds[proxy] = newBrowserId;
-      await saveBrowserIds(browserIds);
-      console.log(`Generated new browser_id for proxy ${proxy}: ${newBrowserId}`);
-      return newBrowserId;
-    }
-  }
-  function getCurrentTimestamp () {
-    return Math.floor(Date.now() / 1000);
-  }
-  async function pingProxy (proxy, browser_id, uid) {
-    const timestamp = getCurrentTimestamp();
-    const pingPayload = { "uid": uid, "browser_id": browser_id, "timestamp": timestamp, "version": "1.0.1" };
+}
 
-    while (true) {
-      try {
-        const pingResponse = await coday('https://api.aigaea.net/api/network/ping', 'POST', pingPayload, proxy);
-        await coday('https://api.aigaea.net/api/network/ip', 'GET', {}, proxy);
-        console.log(`Ping successful for proxy ${proxy}:`, pingResponse);
-
-        if (pingResponse.data && pingResponse.data.score < 50) {
-          console.log(`Score below 50 for proxy ${proxy}, re-authenticating...`);
-          await handleAuthAndPing(proxy);
-          break;
-        }
-      } catch (error) {
-        console.error(`Ping failed for proxy ${proxy}:`, error);
-      }
-      await new Promise(resolve => setTimeout(resolve, 600000));
-    }
+async function authProxy(proxy, browserIdsFilePath, headers) {
+  const data = {}
+  const response = await request(
+    'https://api.aigaea.net/api/auth/session',
+    'POST',
+    data,
+    proxy,
+    headers
+  )
+  if (response && response.data) {
+    const uid = response.data.uid
+    const browserId = await getBrowserIdForProxy(proxy, browserIdsFilePath)
+    console.log(`代理 ${proxy} 验证成功，uid: ${uid}, browser_id: ${browserId}`)
+    pingProxy(proxy, browserId, uid, browserIdsFilePath, headers)
+  } else {
+    console.error(`代理 ${proxy} 的认证失败`)
   }
-  async function handleAuthAndPing (proxy) {
-    const payload = {};
-    const authResponse = await coday("https://api.aigaea.net/api/auth/session", 'POST', payload, proxy);
+}
 
-    if (authResponse && authResponse.data) {
-      const uid = authResponse.data.uid;
-      const browser_id = await getBrowserId(proxy);
-      console.log(`Authenticated for proxy ${proxy} with uid ${uid} and browser_id ${browser_id}`);
-      pingProxy(proxy, browser_id, uid);
-    } else {
-      console.error(`Authentication failed for proxy ${proxy}`);
+async function request(url, method, data = null, proxy, headers) {
+  try {
+    const agent = new HttpsProxyAgent(proxy)
+    const options = {
+      method,
+      headers,
+      agent,
     }
+    if (method === 'POST') {
+      options.body = JSON.stringify(data)
+    }
+    const response = await fetch(url, options)
+    return await response.json()
+  } catch (error) {
+    console.error(`代理出错: ${proxy}`, error)
   }
+}
+
+async function main(accessToken, proxyFilePath, browserIdsFilePath, headers) {
+  headers.Authorization = `Bearer ${accessToken}`
 
   try {
-    const proxyList = await fs.readFile(proxyPath, 'utf-8');
-    const proxies = proxyList.split('\n').map(proxy => proxy.trim()).filter(proxy => proxy);
+    const proxyData = await fs.readFile(proxyFilePath, 'utf-8')
+    const proxies = proxyData
+      .split('\n')
+      .filter((proxy) => proxy.trim())
+      .map((proxy) => proxy.trim())
 
     if (proxies.length === 0) {
-      console.error("No proxies found in" + proxyPath);
-      return;
+      console.error('在 proxy.txt 中未找到代理')
+      return
     }
-
-    for (const proxy of proxies) {
-     await handleAuthAndPing(proxy) 
-    }
+    await Promise.all(proxies.map((proxy) => authProxy(proxy, browserIdsFilePath, headers)))
   } catch (error) {
-    console.error('An error occurred:', error);
+    console.error('发生错误:', error)
   }
-
 }
+
 async function run() {
-  const users = (await fs.readFile('./user.txt','utf-8')).split('\n').filter(item => !!item)
+  const users = (await fs.readFile('./user.txt', 'utf-8'))
+    .split('\n')
+    .filter((item) => !!item)
   for (const index in users) {
-    const [browserId, token] = users[index].split(',')
-    const [id8] = browserId.split('-')
-    console.log('index ----------', index)
+    const [_, token] = users[index].split(',')
     const i = Number(index) + 1
-    await main(token, id8, `./config/${i}.txt`, `./config/${i}.json`)
+
+    const headers = {
+      Accept: 'application/json, text/plain, */*',
+      Connection: 'keep-alive',
+      'Content-Type': 'application/json',
+      'User-Agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36',
+      'User-Agent': fakeUa(),
+      'Accept-Language': getRandomAcceptLanguage(),
+    }
+
+    await main(token, `./config/${i}.txt`, `./config/${i}.json`, headers)
   }
 }
 run()
